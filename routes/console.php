1c11b8cf267a4d4e7ad4d64f069fef99c11540ea
Akrom <akrommuhammadyusuf@gmail.com>
2026-04-09 13:52:47 +0700

    new

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Jalankan cleanup file kadaluarsa setiap hari jam 1 pagi
+Schedule::command('files:cleanup-expired')->dailyAt('01:00');
